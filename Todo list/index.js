fc01e9c7b4744b893bdc1b8c408a4915a0a5c37d
AlainK22 <lainkam22@gmail.com>
2019-08-22 17:35:11 +0200

    updates in todo list

--- Todo list/index.js
+++ Todo list/index.js
@@ -1,17 +1,13 @@
-// todo:
-// 1. get data inputted
-// 2. make sure i display the inputted data
-// 3. create a checkbox
-// 4. hise a checked list item
+//Todo
+//-----------
+//Refact Codes
 
 window.onload = function() {
 
-//Variables
+// Global Variables
 	var form = document.getElementById("form");
 	var input = document.getElementById("input");
 	var btn = document.getElementById("btn");
-	var id = 1;
-	var liItem = "";
 	var todoList = [];
 
 //Event Listeners
@@ -22,38 +18,30 @@ window.onload = function() {
 
     function addTodoItem() 
     {
-
-// We start by validating the entry
         if(input.value == "") 
         {
-			alert("Please enter somethin");
+			alert("Please enter something");
 		}
-		// else if(typeof  =="integer"){
-		// 	alert("Sorry, You can't enter just a number");
-		// }
 		else {
-			var text = input.value;	
-			var item = `<li id="li-${id}">${text}<input id="box-${id}" class="checkboxes" type="checkbox"></li> `;				
+			var text = JSON.parse(JSON.stringify(input.value));	
+			var item = `<li>${text}<input class="checkboxes" type="checkbox"></li> `;				
 			list.insertAdjacentHTML('beforeend', item);	
 		}
 	}
+
 //Displaying the TODO List
 	function displayList() {
 		todoList.forEach(function(element) {
-			console.log(element.item)
 			var text = element.item;
-			var item = `<li id="li-${id}">${text}`;
 		});
 	}
 	//Checking the checkbox and the todo item dissapear!
-	/**
-	 * 
-	 * @param {*} event 
-	 */
+
 	function boxChecked(event) {
 		const element = event.target;
 		if(element.type == "checkbox") {
 			element.parentNode.style.display = "none";
+			alert("One To-Do Item Completed");
 			console.log(text);
 		}
 	}
